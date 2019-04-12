ee602691c0dc3367435aee5ba5971f6b93474e4a
GPlay97 <GPlay97@users.noreply.github.com>
2019-04-12 20:51:31 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>VueFarm </span>
+        <span class="font-weight-light"> Simple, fun farm game</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <Map/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Map from './components/Map'
+
+export default {
+  name: 'App',
+  components: {
+    Map
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/components/Map.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      Wow, this is working!
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    })
+  }
+</script>
+
+<style>
+
+</style>
