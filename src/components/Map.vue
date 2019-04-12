0c223e79670009d0387d46dba8b22c5def2aa251
GPlay97 <GPlay97@users.noreply.github.com>
2019-04-12 22:54:51 +0200

    restructure and preparation for plants

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,21 +1,35 @@
 <template>
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      Wow, this is working!
-    </v-layout>
-  </v-container>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3 :fill-height="true">
+      <v-card>
+        <v-container grid-list-sm fluid>
+          <v-layout row wrap justify-center>
+            <v-flex v-for="plant in plants" :key="plant.name" xs3>
+              <Soil :state="plant.state" :name="plant.name" />
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+  import Soil from './Soil';
+
   export default {
     data: () => ({
-    })
+      plants: [{
+        name: 'soil',
+        state: 0
+      }]
+    }),
+    components: {
+      Soil
+    }
   }
 </script>
 
-<style>
+<style scoped>
 
 </style>
---
+++ src/components/Soil.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-img :src="require(`@/assets/img/${plant.name}_${plant.state}.png`)" aspect-ratio="1" height="64px" width="64px" :contain="true" @click="grow()"></v-img>
+</template>
+
+<script>
+    export default {
+        props: {
+            state: Number,
+            name: String
+        },
+        data: () => ({
+            plant: {
+                state: 0,
+                name: 'soil'
+            }
+        }),
+        methods: {
+            grow() {
+                this.plant.state = 1;
+                this.$emit('growed', this);
+            }
+        },
+        created() {
+            console.log(require(`@/assets/plants/soil.json`));
+            this.plant.state = this.state;
+            this.plant.plant = this.plant;
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
