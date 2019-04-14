ef884fa477ef91b4f8e8aece6e3a4ee16d801f78
GPlay97 <GPlay97@users.noreply.github.com>
2019-04-14 21:06:01 +0200

    new plants; add first handling for growing

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,20 +5,51 @@
         <v-container grid-list-sm fluid>
           <v-layout row wrap justify-center>
             <v-flex v-for="(plant, key) in plants" :key="(plant.name, key)" xs3>
-              <Soil :state="plant.state" :name="plant.name" />
+              <Soil :state="plant.state" :name="plant.name" @unplanted="plantSelector" />
             </v-flex>
           </v-layout>
         </v-container>
       </v-card>
     </v-flex>
+    <v-bottom-sheet v-model="showPlantSelection">
+      <v-list>
+        <v-subheader>Select the plant</v-subheader>
+        <v-list-tile v-for="plant in availablePlants" :key="plant.name">
+          <v-list-tile-avatar>
+            <v-avatar size="32px" tile>
+              <img :src="require(`@/assets/img/${plant.name}.png`)" :alt="plant.name">
+            </v-avatar>
+          </v-list-tile-avatar>
+          <v-list-tile-title>
+            <p v-if="plant.time">{{ plant.name }} ({{ `${plant.time / 60} min.` }})</p>
+            <p v-else>{{ plant.name }}</p>
+          </v-list-tile-title>
+          <v-list-tile-action>
+            <v-btn @click="plantField(plant)">
+              {{ plant.cost  || '-' }}
+              <v-icon>attach_money</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-bottom-sheet>
   </v-layout>
 </template>
 
 <script>
   import Soil from './Soil';
 
+  import PlantPeach from '../assets/plants/peach.json';
+  import PlantPlum from '../assets/plants/plum.json';
+
   export default {
     data: () => ({
+      showPlantSelection: false,
+      currentField: false,
+      availablePlants: [
+        PlantPlum,
+        PlantPeach
+      ],
       plants: [{
         name: 'soil',
         state: 0
@@ -27,9 +58,6 @@
         state: 0
       }, {
         name: 'soil',
-        state: 1
-      }, {
-        name: 'soil',
         state: 0
       }, {
         name: 'soil',
@@ -39,7 +67,10 @@
         state: 0
       }, {
         name: 'soil',
-        state: 1
+        state: 0
+      }, {
+        name: 'soil',
+        state: 0
       }, {
         name: 'soil',
         state: 0
@@ -47,6 +78,16 @@
     }),
     components: {
       Soil
+    },
+    methods: {
+      plantSelector(field) {
+        this.currentField = field;
+        this.showPlantSelection = true;
+      },
+      plantField(plant) {
+        this.currentField.plantField(plant.name);
+        this.showPlantSelection = this.currentField = false;
+      }
     }
   }
 </script>
--- src/components/Soil.vue
+++ src/components/Soil.vue
@@ -1,12 +1,17 @@
 <template>
-    <v-container class="plant-container">
-        <v-img class="plant-img" :src="require(`@/assets/img/${plant.name}_${plant.state}.png`)" aspect-ratio="1"
-            height="64px" margin width="64px" :contain="true" @click="grow()"></v-img>
-        <p v-if="plant.state" class="text-xs-center plant-time">00:55</p>
+    <v-container class="plant-container" @click="plantSelector">
+        <v-img class="plant-img" :src="plantImg" aspect-ratio="1" height="64px" margin width="64px" :contain="true">
+        </v-img>
+        <p v-if="plant.name !== 'soil' && plant.state + 1 <= plant.states.length" class="text-xs-center plant-time">{{ plantTime }}</p>
     </v-container>
 </template>
 
 <script>
+    import {
+        setTimeout,
+        clearInterval,
+        setInterval
+    } from 'timers';
     export default {
         props: {
             state: Number,
@@ -15,17 +20,65 @@
         data: () => ({
             plant: {
                 state: 0,
+                states: [],
+                time: 0,
+                cost: 0,
+                gold: 0,
+                xp: 0,
                 name: 'soil'
-            }
+            },
+            plantTime: 0,
+            plantInterval: 0
         }),
+        computed: {
+            plantImg() {
+                return require(`@/assets/img/${this.plant.name}_${this.plant.state}.png`);
+            }
+        },
         methods: {
-            grow() {
-                this.plant.state = 1;
-                this.$emit('growed', this);
+            setPlant(name) {
+                var plant = require(`@/assets/plants/${name}.json`);
+
+                Object.keys(plant).forEach((key) => this.$set(this.plant, key, plant[key]));
+            },
+            startGrow() {
+                var self = this;
+
+                console.log('start grow');
+
+                setTimeout(() => {
+                    console.log('growed');
+                    self.plant.state++;
+                    if (self.plant.states[self.plant.state]) {
+                        self.startGrow();
+                    } else {
+                        console.log('Fully growed');
+                        self.$emit('growed', this);
+                        clearInterval(self.plantInterval);
+                    }
+                }, self.plant.states[self.plant.state] * 1000);
+
+                if (!self.plantInterval) {
+                    self.plantInterval = setInterval(() => {
+                        self.plantTime = parseInt(new Date().getTime() / 1000) - (self.plant.planted + self
+                            .plant.time);
+                    }, 1000);
+                }
+            },
+            plantSelector() {
+                if (this.plant.name === 'soil') this.$emit('unplanted', this);
+            },
+            plantField(name) {
+                console.log('plant field', name);
+                this.setPlant(name);
+                this.plant.planted = parseInt(new Date().getTime() / 1000);
+                this.plant.state = 0;
+                this.plantTime = this.plant.time;
+                this.startGrow();
             }
         },
         created() {
-            this.$set(this.plant, require(`@/assets/plants/${this.plant.name}.json`));
+            this.setPlant(this.plant.name);
             this.plant.state = this.state;
         }
     }
@@ -35,12 +88,14 @@
     .plant-container {
         padding: 0;
     }
+
     .plant-img {
         margin: auto;
     }
+
     .plant-time {
         background: #616161;
-        box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12);
+        box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
         border-radius: 2px;
         color: #fff;
         opacity: 0.9;
