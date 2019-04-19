8d524042dc95c4a4b797a48a6e0a84df656c9e4f
GPlay97 <GPlay97@users.noreply.github.com>
2019-04-20 00:32:53 +0200

    first save implementation; first level system

--- src/main.js
+++ src/main.js
@@ -12,6 +12,23 @@ new Vue({
     level: 1,
     xp: 0
   }),
+  watch: {
+    money(money) {
+      localStorage.setItem('money', money);
+    },
+    level(level) {
+      localStorage.setItem('level', level);
+    },
+    xp(xp) {
+      localStorage.setItem('xp', xp);
+      if (xp > Math.pow(2, this.level + 1)) this.level++;
+    }
+  },
+  created() {
+    this.money = parseInt(localStorage.getItem('money')) || this.money;
+    this.level = parseInt(localStorage.getItem('level')) || this.level;
+    this.xp = parseInt(localStorage.getItem('xp')) || this.xp;
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
